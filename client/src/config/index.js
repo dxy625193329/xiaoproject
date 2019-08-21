4a56d51a7e3deb09fe0344a052d22e0afcd85a82
cen <317842449@qq.com>
2019-08-21 19:12:14 +0800

    chore: 修复了部分BUG

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -4,7 +4,7 @@ import { PROD_BASE_URL } from './prod.conf'
 let BASE_URL = ''
 
 if (process.env.NODE_ENV === 'development') {
-  BASE_URL = DEV_BASE_URL
+  BASE_URL = PROD_BASE_URL
 } else {
   BASE_URL = PROD_BASE_URL
 }
